511d7ef6a4a4073f2944e89299e2ff7cb966604f
erinleeme <gusdk1793@gmail.com>
2023-07-10 21:26:00 +0900

    Session 로그인 기능 추가

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,21 +1,25 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
+    /*로그인 함수*/
     @ResponseBody
     @PostMapping("/login")
-    public String login(@RequestParam String email, String password) {
-        memberService.login(email, password);
-        return "login success!";
+    public String loginCheck(@RequestParam String email, @RequestParam String password) {
+        log.info("login 정보 : " + email);
+        return memberService.loginCheck(email, password); /*성공하면 success, 실패하면 fail 반환*/
     }
 }
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -5,34 +5,42 @@ import com.example.demo.dao.MemberDAO;
 import com.example.demo.exception.ErrorCode;
 import com.example.demo.exception.IsExistCheckException;
 import com.example.demo.mapper.MemberMapper;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberMapper memberMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final HttpSession session;
 
-    public void login(String email, String password) {
-
-        /*들어온 email로 Member 데이터 조회*/
+    @Transactional
+    public String loginCheck(String email, String password) {
         MemberDAO memberDAO = memberMapper.selectMemberByEmail(email);
 
-        if(memberDAO==null) {
-            throw new IsExistCheckException(ErrorCode.NOT_FOUND_EMAIL);
-        } else{
-            /*있는 경우 요청메세지를 통해 들어온 password와 일치한지 확인*/
-
-
+        /*회원 정보 유무 확인*/
+        if(memberDAO == null) {
+            return "fail";
         }
 
+        /*비밀번호 일치 확인*/
+        if(!bCryptPasswordEncoder.matches(password, memberDAO.getPassword())) {
+            log.info("비밀번호 불일치");
+            return "fail";
+        }
 
+        /*회원 정보가 있을 시 session에 해당 회원 정보를 저장*/
+        session.setAttribute("memberId", memberDAO.getMemberId());
+        session.setAttribute("email", memberDAO.getEmail());
+        session.setAttribute("nickname", memberDAO.getNickname());
 
-
-
+        return "success";
     }
-
-
-
 }
