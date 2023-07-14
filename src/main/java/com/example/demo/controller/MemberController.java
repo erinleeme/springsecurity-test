f4118465f67901cbb191ee559589068ef9cbe6c9
erinleeme <gusdk1793@gmail.com>
2023-07-14 12:42:11 +0900

    spring security jwt 구현

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -26,37 +26,24 @@ public class MemberController {
     @Validated
     @PostMapping("/login")
     public ResponseEntity<JwtTokenDTO> loginCheck(@RequestParam @Email String email, @RequestParam String password) {
-        /*1. 유효성 검사*/
-        /*2. Email로 사용자 조회*/
-        /*3. 아이디랑 비밀번호 일치하는지 확인*/
-        MemberResponseDTO memberResponseDTO = memberService.loginCheck(email, password);
-
-        if(memberResponseDTO == null) {
-            JwtTokenDTO jwtTokenDTO = JwtTokenDTO.builder()
-                    .result("fail")
-                    .accessToken(null)
-                    .refreshToken(null)
-                    .build();
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jwtTokenDTO); /*아이디와 비밀번호 실패시 인증실패 상태 반환*/
-        }
-
-        /*4. 토큰 발급*/
+        /*1. AuthenticationFilter를 통해 ID와 PW 유효성 겁사 진행*/
+        /*2. UserPasswordAuthenticationToken 발급*/
+        /*3. 발급된 Token으로 AuthenticationManger로 보내져 사용자 유효성 검사 진행*/
+        /*3-1. Email로 사용자 조회*/
+        /*3-2. 아이디랑 비밀번호 일치하는지 확인*/
+        /*4. 인증에 성공하면 UserPasswordAuthenticationToken기반으로 JWT 토큰 발급*/
         /*4-1. MemberId, MemberType으로 Access Token 발급*/
-        String accessToken = jwtService.generateAccessToken(memberResponseDTO.getMemberId(), memberResponseDTO.getMemberType());
-
         /*4-2. MemberId, MemberType으로 Refresh Token 발급*/
         /*4-2-1. Refresh Token을 Hashing 처리*/
         /*4-2-2. Refresh Token을 DB에 저장*/
-        String refreshToken = jwtService.generateRefreshToken(memberResponseDTO.getMemberId(), memberResponseDTO.getMemberType());
+        MemberResponseDTO memberResponseDTO = memberService.loginCheck(email, password);
 
         /*5. Token을 저장하여 보내기 */
-        /*성공하면 success, 실패하면 fail 반환*/
-        JwtTokenDTO jwtTokenDTO = JwtTokenDTO.builder()
-                .result("success")
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
 
-        return ResponseEntity.status(HttpStatus.OK).body(jwtTokenDTO);
+
+        /*성공하면 success, 실패하면 fail 반환*/
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(JwtTokenDTO);
     }
 }
